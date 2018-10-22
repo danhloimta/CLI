6e27a24a6f802cae546533facb88093179394888
vanhaile1798 <vhail17.9.98@gmail.com>
2018-10-22 18:16:59 +0700

    Added showing, updating, filtering, paginating functionality

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let readlineSync = require('readline-sync');
 
 let Table = require('cli-table');
 
-let fs = require('fs');
+
+
+// let fs = require('fs');
 
 const low = require('lowdb');
 
@@ -12,16 +14,175 @@ const adapter = new FileSync('./db/db1.json');
 const db = low(adapter);
 
 db.defaults({students: []})
-	.write();
+.write();
 
 // let students = fs.readFileSync('./db/db.json', {encoding: 'utf8'}) || [];
 // let students = fs.readFileSync('./db/db1.json', {encoding: 'utf8'}) || [];
 
 let students = db.get('students').value();
 
-function Helper(searchBy) {
+let options = [
+	{
+		option: 'show',
+		description: 'Show the list of students'
+	},
+	{
+		option: 'add',
+		description: 'Add a student'
+	},
+	{
+		option: 'update',
+		description: 'Update a student'
+	},
+	{
+		option: 'delete',
+		description: 'Delete a student'
+	},
+	{
+		option: 'sort',
+		description: 'Sort the list of students'
+	},
+	{
+		option: 'filter',
+		description: 'Filter students'
+	},
+	{
+		option: 'paginate',
+		description: 'Pagination'
+	},
+	{
+		option: 'exit',
+		description: 'Close the app'
+	}
+
+];
+
+
+
+const under18 = 1;
+const above18 = 2;
+
+const filterOptions = [
+	{
+		key: under18,
+		description: "Get student under 18"
+	},
+	{
+		key: above18,
+		description: "Get student equal and above 18"
+	}
+
+]
+
+// Function for use
+
+function Helper(searchItem, value) {
+	searchItem = searchItem.toLowerCase();
+	return students.filter((item, index) => {
+		if (searchItem === 'phone number') {
+			return item.phoneNumber == value;
+		}
+		if (searchItem === 'group name') {
+			return item.groupName == value;
+		}
+		return item[searchItem] === value;
+	});
+}
+
+function FilterStudent(option) {
+
+	let student = [];
+	if (option === 1) {
+		student = students.filter((item) => {
+			return item.age < 18;
+		});
+		ShowTable(student);
+	} else if (option === 2) {
+		student = students.filter((item) => {
+			return item.age >= 18;
+		});
+		ShowTable(student); 
+	} else if (option === 0) {
+		console.log('Done');
+		return;
+	}
+}
+
+function Sort(typeOfSort, sortBy, students) {
+
+	switch (typeOfSort) {
+					case 'ascending':
+					if (sortBy === 'name') {
+						students.sort((a, b) => {
+							if (a.name.toLowerCase() < b.name.toLowerCase()) {
+								return -1;
+							} else {
+								return 1;
+							}
+							return 0;
+						});
+					}
+
+					if (sortBy === 'appearance') {
+						students.sort((a, b) => {
+							if (a.appearance < b.appearance) {
+								return -1;
+							} else {
+								return 1;
+							}
+							return 0;
+						});
+					}
+					break;
+
+					case 'descending':
+					if (sortBy === 'name') {
+						students.sort((a, b) => {
+							if (a.name.toLowerCase() > b.name.toLowerCase()) {
+								return -1;
+							} else {
+								return 1;
+							}
+							return 0;
+						});
+					}
+
+					if (sortBy === 'appearance') {
+						students.sort((a, b) => {
+							if (a.appearance > b.appearance) {
+								return -1;
+							} else {
+								return 1;
+							}
+							return 0;
+						});
+					}
+					break;
+				}
 
 }
+
+function DefaultInput(text, defaultValue) {
+	return readlineSync.question(text, {
+		defaultInput: defaultValue
+	});
+}
+
+function ShowTable(arr) {
+	let table = new Table({
+				head: ['Name', 'Age', 'Sex', 'Group Name', 'Phone Number', 'Skills', 'Email', 'Appearance']
+			});
+	arr.forEach((item) => {
+		let student = [];
+		for (let value in item) {
+			student.push(item[value]);
+		}
+		table.push(student);
+	});
+
+	console.log(table.toString());
+}
+
 
 
 function CreateStudent(name, age, sex, groupName, phoneNumber, skills, email, appearance) {
@@ -35,171 +196,224 @@ function CreateStudent(name, age, sex, groupName, phoneNumber, skills, email, ap
 	this.appearance = appearance;
 }
 
-
-function app() {
-	let table = new Table({
-		head: ['Name', 'Age', 'Sex', 'Group Name', 'Phone Number', 'Skills', 'Email', 'Appearance']
-	});
-	
+function Add() {
+	let studentPhoneNumber = [];
+	let studentEmail = [];
 	students.forEach((item) => {
-		let student = [];
-		for (let value in item) {
-			student.push(item[value]);
+		studentPhoneNumber.push(item.phoneNumber);
+		studentEmail.push(item.email);
+	});
+	let phoneNumber;
+	let email;
+	let sex;
+
+	let name = readlineSync.question('Enter name: ');
+	let age = readlineSync.questionInt('Enter age: ');
+
+	do {
+		sex = readlineSync.question('Enter sex(M/F): ');
+		if (sex !== "M" && sex !== "F") {
+			console.log("Please try again");
+		}
+	} while (sex !== "M" && sex !== "F");
+
+	let groupName = readlineSync.question('Enter group: ');
+
+	do {
+		phoneNumber = readlineSync.questionInt('Enter phone number: ');
+
+		if (phoneNumber.toString().length < 5 || phoneNumber.toString().length > 12) {
+			console.log('Invalid phone number');
+		}
+		if (studentPhoneNumber.includes(phoneNumber)) {
+			console.log('Phone number existed');
 		}
 
-		table.push(student);
+	} while (studentPhoneNumber.includes(phoneNumber) || phoneNumber.toString().length < 5 || phoneNumber.toString().length > 12);
+
+	let	skills = readlineSync.question('Enter skill(s): ');
+
+	do {
+		email = readlineSync.questionEMail('Enter email: ');
+	} while (studentEmail.includes(email));
+
+	let appearance = readlineSync.questionInt('Enter appearance: ');
+
+	let student = new CreateStudent(name, age, sex, groupName, phoneNumber, skills, email, appearance);
+
+		// fs.writeFileSync('./db/db.json', JSON.stringify(students));
+	db.get('students')
+	.push(student)
+	.write();
+	console.log('Added successfully\n');
+}
 
 
+//////////////////////////////////////////////////////////////
+
+function App() {
+
+	// db.get('students').write().forEach((item) => {
+	// 	console.log(item);
+	// });
+
+	// db.get('students').write().sort((a, b) => {
+	// 	if (a.age < b.age) {
+	// 		return -1;
+	// 	} else {
+	// 		return 1;
+	// 	}
+	// 	return 0;
+	// });
+
+	// console.log(db.get('students').write());
+	console.log('Instructions!!!');
+
+	options.forEach((item) => {
+		console.log(`Enter [${item.option}] : ${item.description}`);
 	});
-	if (students.length !== 0) {
-		console.log(table.toString());
-	}
-	
 
-	let choice = readlineSync.question('What do you want to do: ');
+	
+	let choice = readlineSync.question('\nWhat do you want to do: ');
+
 	switch (choice) {
 
+		case 'show':
+			ShowTable(students);
+			break;
+			
 		case 'add':
 
-			let studentPhoneNumber = [];
-			let studentEmail = [];
-			students.forEach((item) => {
-				studentPhoneNumber.push(item.phoneNumber);
-				studentEmail.push(item.email);
-			});
-			let phoneNumber;
-			let email;
-			let sex;
-
-			let name = readlineSync.question('Enter name: ');
-			let age = readlineSync.questionInt('Enter age: ');
-			do {
-				sex = readlineSync.question('Enter sex(M/F): ');
-				if (sex !== "M" && sex !== "F") {
-					console.log("Please try again");
-				}
-			} while (sex !== "M" && sex !== "F");
-			
-			let groupName = readlineSync.question('Enter group: ');
-			
-			do {
-				phoneNumber = readlineSync.questionInt('Enter phone number: ');
-				if (phoneNumber < 10 || phoneNumber > 12) {
-					console.log('Invalid phone number');
-				}
-				if (studentPhoneNumber.includes(phoneNumber)) {
-					console.log('Phone number existed');
-				}
-			} while (studentPhoneNumber.includes(phoneNumber) && phoneNumber < 10 && phoneNumber > 12);
-
-			let	skills = readlineSync.question('Enter skill(s): ');
-			
-			do {
-				email = readlineSync.questionEMail('Enter email: ');
-			} while (studentEmail.includes(email));
-			
-			let appearance = readlineSync.questionInt('Enter appearance: ');
-			let student = new CreateStudent(name, age, sex, groupName, phoneNumber, skills, email, appearance);
-			
-			// fs.writeFileSync('./db/db.json', JSON.stringify(students));
-			db.get('students')
-				.push(student)
-				.write();
-			console.log('Added successfully');
+			Add();
 			break;
 
 
 		case 'update':
-			let update = readlineSync.question('Search item to update by: ');
+			let update = readlineSync.questionInt('Search item to update by phone number: ');
 
-			if (update === "phone number") {
-				let number = readlineSync.question('Enter phone number');
+			let index = 0;
 
+			let updateElement = students.find((item, ind) => {
+				index = ind;
+				return item.phoneNumber === update;
+			});
 
-			} else {
-				let email = readlineSync.question('Enter email: ');
-			}
+			let name = DefaultInput('New name: ', updateElement.name);
+			let age = DefaultInput('New age: ', updateElement.age);
+			let sex = DefaultInput('New sex: ', updateElement.sex);
+			let groupName = DefaultInput('New group name: ', updateElement.groupName);
+			let phoneNumber = DefaultInput('New phone number: ', updateElement.phoneNumber);
+			let skills = DefaultInput('New skills: ', updateElement.skills);
+			let email = DefaultInput('New email: ', updateElement.email);
+			let appearance = DefaultInput('New appearance: ', updateElement.appearance);
+
+			updateElement = {
+				name,
+				age,
+				sex,
+				groupName,
+				phoneNumber,
+				skills,
+				email,
+				appearance
+			};
+
+			console.log(updateElement);
+			students[index] = updateElement;
 			break;
 
 
 		case 'delete':
 
-			let phoneNum = parseInt(readlineSync.question('Search an item to delete by phone number: '));
+			let phoneNum = readlineSync.questionInt('Search an item to delete by phone number: ');
 			for (let student in students) {
-									
 
 				if (students[student].phoneNumber === phoneNum) {
-						students.splice(student, 1);
+					students.splice(student, 1);
 				}
 				console.log('Deleted successfully');
 			}
 			break;
 			
-		case 'sort':
-			let typeOfSort = readlineSync.question('Sort: ');
-			let sortBy = readlineSync.question('Sort by: ');
+		case 'sort': // Type of sort: Ascending and Descending, support for name and appearance
+			let typeOfSort = readlineSync.question('Sort (ascending/descending): ');
+			let sortBy = readlineSync.question('Sort by (name/appearance): ');
 
-			switch (typeOfSort) {
-				case 'ascending':
-					if (sortBy === 'name') {
-						students.sort((a, b) => {
-							if (a.name < b.name) {
-								return -1;
-							} else {
-								return 1;
-							}
-						});
-					}
+			let studentArray = [];
+			students.forEach((item) => {
+				studentArray.push(item);
+			})
+			Sort(typeOfSort, sortBy, studentArray);
 
-					if (sortBy === 'appearance') {
-						students.sort((a, b) => {
-							if (a.appearance < b.appearance) {
-								return -1;
-							} else {
-								return 1;
-							}
-						});
-					}
-					break;
+			// console.log('RESULT');
 
-				case 'descending':
-					if (sortBy === 'name') {
-						students.sort((a, b) => {
-							if (a.name > b.name) {
-								return -1;
-							} else {
-								return 1;
-							}
-						});
-					}
+			ShowTable(studentArray);
 
-					if (sortBy === 'appearance') {
-						students.sort((a, b) => {
-							if (a.appearance > b.appearance) {
-								return -1;
-							} else {
-								return 1;
-							}
-						});
-					}
-					break;
-		}
-		break;
-	case 'search':
-		let searchItem = readlineSync.question('Search for an item by: ');
-		Helper(searchItem);
+			break;
 
-		break;
-	case 'filter':
-	break;
-	case 'page':
-	break;
-	case 'exit':
-	return;
+		case 'search': // search by all properties
+
+			let searchItem = readlineSync.question('Search for an item by: ');
+
+			let value = readlineSync.question('Enter ' + searchItem.toLowerCase()  + ': ');
+
+
+
+			let searchStudents = Helper(searchItem, value);
+
+			ShowTable(searchStudents);
+
+
+			break;
+		case 'filter': // Only support for filtering by age (under 18 or equal and above 18)
+			
+			filterOptions.forEach((item) => {
+				console.log('['+ item.key + '] ' + item.description);
+			});
+
+			function Filter() {
+				let option = readlineSync.questionInt('Your choice: ');
+
+				FilterStudent(option);
+				if (option === 0) {
+					return;
+				}
+				Filter();
+			}
+
+			Filter()
+
+			break;
+
+		case 'paginate': // 3 Students per page
+
+			let perPage = 3;
+			let totalPage = Math.ceil(students.length / perPage);
+			console.log('Total page: ' + totalPage);
+			function paginate() {
+				let page = DefaultInput('Enter page: ', 1);
+
+				if (page == 0) {
+					return;
+				}
+				let start = (page - 1) * perPage;
+				let end = page * perPage;
+
+				let newArr = students.slice(start, end);
+				ShowTable(newArr);
+				paginate();
+			}
+
+			paginate();
+			
+
+			break;
+		case 'exit':
+			return;
+	}
+	App();
 }
-app();
-}
 
-app();
+	App();
 
 
